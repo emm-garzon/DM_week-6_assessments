4676d017c5921913ae18e955197b96070432e222
Emmanuel Garzon <eggarzon@gmail.com>
2022-04-29 11:29:53 -0700

    tests added to functions.test.ts

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,16 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require("./utils");
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-})
+let testData = [1, 5, 34, 17, 25];
+
+describe("shuffleArray tests", () => {
+  test("shuffleArray returns an array containing the same values", () => {
+    let testArray = shuffleArray(testData);
+    expect(shuffleArray(testArray)).toEqual(expect.arrayContaining(testData));
+  });
+
+  test("shuffleArray returns an array of the same length", () => {
+    let lengthOne = testData.length;
+    let lengthTwo = shuffleArray(testData).length;
+    expect(lengthOne).toEqual(lengthTwo);
+  });
+});
