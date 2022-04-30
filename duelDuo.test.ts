fb44e8e18237cdcbfdfef8a9128c8cf34f137395
Emmanuel Garzon <eggarzon@gmail.com>
2022-04-29 21:16:52 -0700

    automated testing added

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,3 +17,35 @@ test("Title shows up when page loads", async () => {
   const displayed = await title.isDisplayed();
   expect(displayed).toBe(true);
 });
+
+test('clicking DRAW button displays div with id of "choices"', async () => {
+  await driver.findElement(By.id("draw")).click();
+  const choices = await driver.findElement(By.id("choices"));
+  const displayed = await choices.isDisplayed();
+  expect(displayed).toBe(true);
+});
+
+test("clicking 'add to duo' button displays div with id of 'player-duo'", async () => {
+  await driver.findElement(By.id("draw")).click();
+  await driver
+    .findElement(By.xpath('(//button[text()="Add to Duo"])[1]'))
+    .click();
+  const playerDuo = await driver.findElement(By.id("player-duo"));
+  const displayed = await playerDuo.isDisplayed();
+  expect(displayed).toBe(true);
+});
+
+test("when bot is removed from duo, it returns to choices", async () => {
+  await driver.findElement(By.id("draw")).click();
+  await driver
+    .findElement(By.xpath('(//button[text()="Add to Duo"])[1]'))
+    .click();
+  await driver
+    .findElement(By.xpath('(//button[text()="Remove from Duo"])'))
+    .click();
+  const returnedCard = await driver.findElement(
+    By.xpath('(//div[@class="bot-card outline"][5])')
+  );
+  const displayed = await returnedCard.isDisplayed();
+  expect(displayed).toBe(true);
+});
